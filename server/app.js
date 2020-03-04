1c46d39b25ddc919467e364271e1a7e6041b618f
Thomas Matlock <snaxbags@protonmail.com>
2020-03-04 10:11:49 -0600

    Add Routes Object

--- server/app.js
+++ server/app.js
@@ -3,31 +3,33 @@ const http = require('http')  //Loads HTTP Module
 const host = '127.0.0.1'
 const port = 3000
 
-function index (req, res) {
-  res.writeHead(200)
-  res.end('Hello, World')
+//Store Routes and create them for later use in a callback
+let routes = {
+
+  //INDEX ROUTE
+  '/': function index(req, res) {
+        res.writeHead(200)
+        res.end('Hello, World!')
+  },
+  //FOO ROUTE
+  '/foo': function foo (req, res) {
+    res.writeHead(200)
+    res.end('You are now viewing foo')
+  }
 }
 
 //Create HTTP server
 const server = http.createServer((req, res, next) => {
 
-  if (req.url === '/') {
-    return index(req, res)
+  if (req.url in routes) {
+    return routes[req.url](req, res)
   }
 
   res.writeHead(404)
   res.end(http>STATUS_CODES[404])
 
-  // //Tell browser everthing is okay and data is in plain text
-  // res.writeHead(200, {
-  //   'Content-Type': 'text/plain'
-  // })
-
-  // //Write Hello to the page
-  // res.write("hello")
-
   //Tell the server all of the response headers and body have sent
   res.end()
 })
 
-server.listen(port) //Tells which port to listen on
+server.listen(process.env.PORT || port) //Tells which port to listen on
