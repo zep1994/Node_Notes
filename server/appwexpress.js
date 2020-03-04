81dc8c0cf65b559d60bc7b12f31b1ce7ff94e456
Thomas Matlock <snaxbags@protonmail.com>
2020-03-04 13:30:21 -0600

    Add static for public

--- server/appwexpress.js
+++ server/appwexpress.js
@@ -9,10 +9,12 @@ const app = express()
 const PORT = process.env.PORT || 3000
 const HOST = '127.0.0.1'
 
-
 //Set Templating Engine
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
+
+//Setup to use STATIC FILES
+app.use(express.static('public'))
 
 app.get('/', (req, res, next) => {
   let info = {
