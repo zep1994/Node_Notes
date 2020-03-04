00ea517a1361fa2cb8ae7cf976f96abd5638549d
Thomas Matlock <snaxbags@protonmail.com>
2020-03-04 14:55:18 -0600

    Using middleware and multiple matching routes

--- server/appwexpress.js
+++ server/appwexpress.js
@@ -1,6 +1,8 @@
 //Import the top-level function of express
 const express = require('express')
 const cors = require('cors') //Use cors for cross-origin sharing
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 //Creates an Express application using the top-level function
 const app = express()
@@ -16,9 +18,29 @@ app.set('views', 'src/views')
 //Setup to use STATIC FILES
 app.use(express.static(__dirname + '/public'))
 
-
 //make use of cors
 app.use(cors())
+
+//Make use of BODY PARSER
+app.use(bodyParser.json()) //for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) //for parsing application/x-www-form-urlencoded
+
+//Make use of COOKIE PARSER
+app.use(cookieParser()) //module for parsing cookies
+
+//BANANAS ROUTE
+app.get('/bananas', (req, res, next) => {
+  getMember((err, memeber) => {
+    if (err) return next(err)
+    //If no memeber, dont look
+    //just render page now
+    if (!memeber) return next('route')
+    //otherwise, call the next middleware and fetch
+    //the members data
+    req.member = member
+    next()
+  })
+})
 
 //INDEX ROUTE
 app.get('/', (req, res, next) => {
