c2dc698674d69e61cc4649dd2922a8ec4ea96c87
Thomas Matlock <snaxbags@protonmail.com>
2020-03-04 10:04:56 -0600

    Add nodemon and test debug

--- server/app.js
+++ server/app.js
@@ -3,16 +3,28 @@ const http = require('http')  //Loads HTTP Module
 const host = '127.0.0.1'
 const port = 3000
 
+function index (req, res) {
+  res.writeHead(200)
+  res.end('Hello, World')
+}
+
 //Create HTTP server
 const server = http.createServer((req, res, next) => {
 
-  //Tell browser everthing is okay and data is in plain text
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
+  if (req.url === '/') {
+    return index(req, res)
+  }
 
-  //Write Hello to the page
-  res.write("hello")
+  res.writeHead(404)
+  res.end(http>STATUS_CODES[404])
+
+  // //Tell browser everthing is okay and data is in plain text
+  // res.writeHead(200, {
+  //   'Content-Type': 'text/plain'
+  // })
+
+  // //Write Hello to the page
+  // res.write("hello")
 
   //Tell the server all of the response headers and body have sent
   res.end()
