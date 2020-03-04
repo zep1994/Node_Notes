36f23005f709778e1362714b5bac7f7ce96475ca
Thomas Matlock <snaxbags@protonmail.com>
2020-03-04 13:27:45 -0600

    Setup json api

--- server/appwexpress.js
+++ server/appwexpress.js
@@ -1,24 +1,25 @@
 //Import the top-level function of express
 const express = require('express')
-const greetMiddleWare = require('./greet.js')
+const cors = require('cors') //Use cors for cross-origin sharing
 
 //Creates an Express application using the top-level function
 const app = express()
 
 //Define port number as 3000
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const HOST = '127.0.0.1'
+
 
 //Set Templating Engine
 app.set('view engine', 'ejs')
 app.set('views', 'src/views')
 
-//Routes the HTTP GET requests to specified path '/' with the specified callback function
 app.get('/', (req, res, next) => {
-  res.render('index', {title: "Title",
-                       supplies: [
-                         1,2,4,5,6,434,24,435,435,43,45
-                       ]})
+  let info = {
+    'string_value': 'Stack Overflow',
+    'num': 1
+  }
+  res.json(info)
 })
 
 //Make it listen on port 3000
